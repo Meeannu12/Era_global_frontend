55f85cccde099311867b6a3d478e227884d758c4
AnuragLovesCode <anurag.neet01@gmail.com>
2026-01-09 16:17:59 +0530

    add wallet amount show on hover

--- src/pages/Admin/PinManagement.jsx
+++ src/pages/Admin/PinManagement.jsx
@@ -1125,9 +1125,21 @@ const PinManagement = () => {
                       </thead>
                       <tbody>
                         {withdrawTransaction.map((tx) => (
-                          <tr key={tx._id} className="border-b border-gray-700">
-                            <td className="px-1 py-1 text-center">
-                              $ {tx.amount}
+                          <tr key={tx._id} className="group border-b border-gray-700">
+                            <td className="relative px-1 py-1 text-center">
+                              <span
+                                className="
+      absolute left-full ml-2 top-1/2 -translate-y-1/2
+      opacity-0
+      group-hover:opacity-100
+      transition
+      bg-black text-white text-xs px-2 py-1 rounded
+      whitespace-nowrap
+    "
+                              >
+                                {user.name}
+                              </span>
+                              $ {tx?.user?.walletEarning}
                             </td>
                             <td className="px-1 py-1 text-center">{tx.mode}</td>
                             <td className="px-1 py-1 text-center">
@@ -1148,7 +1160,7 @@ const PinManagement = () => {
                             <td className="px-1 py-1 text-center">
                               {new Date(tx.createdAt).toLocaleString()}
                             </td>
-                            <td className="px-1 py-1 flex gap-2 justify-center">
+                            <td className="px-1 py-1 flex gap-2 justify-center hover:{${user.id}">
                               <button
                                 onClick={() =>
                                   handelUpdateStatus(
